14f6d31a7af85f2db97283d94461f52fea2f077b
Kevin Francis <kfrancis@gmail.com>
2014-10-16 11:01:01 +0000

    User model + has_secure_password

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+    has_secure_password
+    
+    validates :name, presence: true
+    validates :email, uniqueness: true
+    validates :password, length: { minimum: 6 }
+    validates :password_confirmation, presence: true
+end

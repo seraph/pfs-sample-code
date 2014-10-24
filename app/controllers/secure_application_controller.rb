aa1f7815409f9c238a2088ad75e9b24ba5b2fc33
Kevin Francis <kfrancis@gmail.com>
2014-10-24 15:39:17 +0800

    Added a new SecureApplicationController to inherit for secure paths, such as /users, which require a login Included the module SessionsHelper, allowing us to check login.

---
+++ app/controllers/secure_application_controller.rb
@@ -0,0 +1,14 @@
+class SecureApplicationController < ApplicationController
+  include SessionsHelper
+    
+  before_action :require_login
+ 
+  private
+ 
+  def require_login
+    unless logged_in?
+      flash[:error] = "You must be logged in to access this section"
+      redirect_to login_path # halts request cycle
+    end
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,4 @@
-class UsersController < ApplicationController
+class UsersController < SecureApplicationController
     
     def index
         if params[:name]
